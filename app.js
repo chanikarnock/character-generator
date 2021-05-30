daa4a50d648c510848d0bca1ef0a95a62ed7a103
chanikarnock <chanikarnock@gmail.com>
2021-05-30 18:08:03 +0700

    update

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ const MAIN4DATA = Object.values(DATA)
 const randomBTN = document.getElementById("firstrandom_btn")
 const resultPara = document.getElementsByClassName("randomResult")
 const detailResult = document.getElementById("detail-result")
+const secRandBTB = document.getElementById("secrandom_btn")
+const resultTriviaPara = document.getElementsByClassName("randomResult2")
 
 randomBTN.addEventListener('click', random4)
+secRandBTB.addEventListener('click', mixRandom)
 
 function random4(){
     detailResult.style.display = "block"
@@ -29,6 +32,10 @@ function random4(){
         }else
             resultPara[i].innerHTML = MAIN4DATA[i][Object.keys(MAIN4DATA[i])[Math.floor(Math.random()*Object.keys(MAIN4DATA[i]).length)]]        
     }       
+}
+
+function mixRandom(){
+    
 }
 
 if ('serviceWorker' in navigator) {
