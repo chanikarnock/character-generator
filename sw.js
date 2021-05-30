5633f2c223dbead11149c0f162c120dc7ddda2e9
chanikarnock <chanikarnock@gmail.com>
2021-05-30 18:21:31 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,9 +4,6 @@ if (workbox) {
     console.log("Yay! Workbox is loaded !");
     workbox.precaching.precacheAndRoute([]);
 
-    /*  cache images in the e.g others folder; edit to other folders you got
-    and config in the sw-config.js file
-    */
     workbox.routing.registerRoute(
         /(.*)others(.*)\.(?:png|gif|jpg)/,
         new workbox.strategies.CacheFirst({
@@ -19,15 +16,11 @@ if (workbox) {
             ]
         })
     );
-    /* Make your JS and CSS âš¡ fast by returning the assets from the cache,
-     while making sure they are updated in the background for the next use.
-    */
+    
     workbox.routing.registerRoute(
-    // cache js, css, scc files
-        /.*\.(?:css|js|scss|)/,
-        // use cache but update in the background ASAP
+   
+        /.*\.(?:css|js|scss|)/,        
         new workbox.strategies.StaleWhileRevalidate({
-            // use a custom cache name
             cacheName: "assets",
         })
     );
@@ -44,13 +37,8 @@ if (workbox) {
             ],
         })
     );
-
-    // add offline analytics
+    
     workbox.googleAnalytics.initialize();
-
-    /* Install a new service worker and have it update
-   and control a web page as soon as possible
-    */
 
     workbox.core.skipWaiting();
     workbox.core.clientsClaim();
