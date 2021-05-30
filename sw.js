31c0ec3e1993a5c5881189826add7107f5925423
chanikarnock <chanikarnock@gmail.com>
2021-05-30 18:16:24 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,6 +4,9 @@ if (workbox) {
     console.log("Yay! Workbox is loaded !");
     workbox.precaching.precacheAndRoute([]);
 
+    /*  cache images in the e.g others folder; edit to other folders you got
+    and config in the sw-config.js file
+    */
     workbox.routing.registerRoute(
         /(.*)others(.*)\.(?:png|gif|jpg)/,
         new workbox.strategies.CacheFirst({
@@ -11,19 +14,25 @@ if (workbox) {
             plugins: [
                 new workbox.expiration.Plugin({
                     maxEntries: 50,
-                    maxAgeSeconds: 30 * 24 * 60 * 60,
+                    maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
                 })
             ]
         })
     );
-
+    /* Make your JS and CSS âš¡ fast by returning the assets from the cache,
+     while making sure they are updated in the background for the next use.
+    */
     workbox.routing.registerRoute(
-        /.*\.(?:css|js|scss|)/,        new workbox.strategies.StaleWhileRevalidate({
-
+    // cache js, css, scc files
+        /.*\.(?:css|js|scss|)/,
+        // use cache but update in the background ASAP
+        new workbox.strategies.StaleWhileRevalidate({
+            // use a custom cache name
             cacheName: "assets",
         })
     );
 
+    // cache google fonts
     workbox.routing.registerRoute(
         new RegExp("https://fonts.(?:googleapis|gstatic).com/(.*)"),
         new workbox.strategies.CacheFirst({
@@ -36,7 +45,12 @@ if (workbox) {
         })
     );
 
+    // add offline analytics
     workbox.googleAnalytics.initialize();
+
+    /* Install a new service worker and have it update
+   and control a web page as soon as possible
+    */
 
     workbox.core.skipWaiting();
     workbox.core.clientsClaim();
